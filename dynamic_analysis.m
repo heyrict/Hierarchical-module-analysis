e3beeaf2faecc34bc7e336254f39d363b0c5488f
heyrict <xiezh0831@yahoo.co.jp>
2023-02-10 13:54:25 +0800

    Alter code for use in svci

--- dynamic_analysis.m
+++ dynamic_analysis.m
@@ -1,28 +1,63 @@
 clc;clear;close all
 %% This function generates the temporally dynamic Hin and Hse, and then calibrates them within individuals. 
-Subj =textread('E:\Data\HCP\Subj_list.txt','%s');
-N_sub=length(Subj);N=360;Total=1200;width=83;
+N = 200;
+basedir="/tmp/data/signal/"
+outputdir="/tmp/data/output/"
+
+subjs = load(strcat(outputdir, "subjects_static.mat")).subjs
+
+fmridir = strcat(basedir, "par", sprintf("%d", N), "/")
+N_sub=length(subjs);
+SeqLen=200;width=100;step=20;TR=2
+subnames={};
 %%%===============================================
 % mypool=parpool('local',24,'IdleTimeout',240);
-S=load('corrected_HB.mat');
-Z=[];
+S=load(strcat(outputdir, 'corrected_Hb_static.mat'));
+Z=[];Hins=[];Hses=[];cHins=[];cHses=[];
+
+if !isfolder(outputdir)
+    mkdir(outputdir);
+end
+
 for sub=1:N_sub;
-    path=strcat('E:\Data\HCP\fMRI\',Subj(sub),'_ROI_ts.mat');
-    MRI=load(char(path));
-    BOLD=[];
-    for s=1:4
-        BOLD=[BOLD,MRI.ROI_ts{1,s}];
-    end   
+    % Retrieve subject id from string
+    subj = char(subjs(sub));
+    if !length(regexp(subj, "^sub.*\\.mat$", "once"))
+        continue;
+    end
+    subfile = strcat(fmridir, subj)
+    subname = regexprep(subj, "^(sub\\d+).*$", "$1");
+    subnames = [subnames, subname];
+
+    fmri = load(subfile).ROI_ts;
+
     IN=[];IM=[];
-    parfor t=1:Total*4-width
-        subdata=BOLD(:,t:t+width);
-        FC=corr(subdata');
+
+    % Use for parallelized computing
+    %function [Hin, Hse] = CalcHStep(t)
+    %    subdata = fmri(t:t+width, :);
+    %    FC = corr(subdata);
+    %    [Clus_num, Clus_size] = Functional_HP(FC, N);
+    %    [Hin, Hse] = Balance(FC, N, Clus_size, Clus_num);
+    %end
+
+    parfor t=1:step:SeqLen-width
+        subdata=fmri(t:t+width, :);
+        FC=corr(subdata);
         [Clus_num,Clus_size] = Functional_HP(FC,N);
         [Hin,Hse] =Balance(FC,N,Clus_size,Clus_num);
-        IN=[IN;Hin];IM=[IM;Hse];
+        IN=[IN,Hin];IM=[IM,Hse];
     end
-    [Hin] = individual_correction(IN,S.x(sub));
-    [Hse] = individual_correction(IM,S.y(sub));
-    [Fre,DIn,DSe,In_time,Se_time] = Flexible(Hin-Hse,0.72)%% calculating dynamic measures
+    Hins=[Hins;IN];
+    Hses=[Hses;IM];
+    [Hin] = individual_correction(IN,S.Hin(sub));
+    [Hse] = individual_correction(IM,S.Hse(sub));
+    cHins=[cHins;Hin];
+    cHses=[cHses;Hse];
+    [Fre,DIn,DSe,In_time,Se_time] = Flexible(Hin-Hse, TR)%% calculating dynamic measures
     Z=[Z;Fre,DIn,DSe,In_time,Se_time];
 end
+
+%save("-7", strcat(outputdir, "subjects_dynamic.mat"), 'subnames');
+save("-7", strcat(outputdir, "Hb_dynamic.mat"), 'Hins', 'Hses', 'cHins', 'cHses');
+save("-7", strcat(outputdir, "measures_dynamic.mat"), 'Z', 'SeqLen', 'width', 'step', 'TR');
--- static_analysis.m
+++ static_analysis.m
@@ -1,31 +1,53 @@
 clc;clear;close all
 %% Static analysis: This function calculates the calibrated Hin, Hse and HB from fMRI time series 
-Subj =textread('E:\Data\HCP\Subj_list.txt','%s');
-N_sub=length(Subj);N=360;
-Long_fmri=[];IN=[];IM=[];
-%mypool=parpool('local',24,'IdleTimeout',240);
-parfor sub=1:N_sub
-    path=strcat('E:\Data\HCP\fMRI\',Subj(sub),'_ROI_ts.mat');
-    MRI=load(char(path));
-    fmri=[];
-    for i=1:4
-        fmri=[fmri;MRI.ROI_ts{1,i}']; 
+N=200;
+%basedir="/run/media/heyrict/wd22a/research/blackblood/signal/"
+basedir="/tmp/data/signal/"
+outputdir="/tmp/data/output/"
+
+fmridir = strcat(basedir, "par", sprintf("%d", N), "/")
+subjs = dir(fmridir);
+N_sub=length(subjs);
+Long_fmri=[];IN=[];IM=[];subnames={};
+truesubjs = {}
+for sub=1:N_sub % Loop through all files in directory
+    % Retrieve subject id from string
+    subj = subjs(sub).name;
+    if !length(regexp(subj, "^sub.*\\.mat$", "once"))
+        continue;
     end
+    truesubjs = [truesubjs, subj]
+end
+subjs = truesubjs
+
+if !isfolder(outputdir)
+    mkdir(outputdir);
+end
+save("-7", strcat(outputdir, "subjects_static.mat"), 'subjs');
+
+%mypool=parpool('local',24,'IdleTimeout',240);
+parfor sub=1:length(subjs)
+    subj = char(subjs(sub))
+    subfile = strcat(fmridir, subj)
+    subname = regexprep(subj, "^(sub\\d+).*$", "$1");
+    subnames = [subnames, subname];
+
+    fmri = load(subfile).ROI_ts;
     Long_fmri=[Long_fmri;fmri];
     %% individual static FC matrix and its hierarchical module partition
     FC=corr(fmri);
     [Clus_num,Clus_size,mFC] = Functional_HP(FC,N);
-    parsave(Subj(sub),Clus_size,Clus_num,'_Clus.mat')
-    parsave(Subj(sub),FC,mFC,'_FC.mat')
+    %parsave(subname,Clus_size,Clus_num,'_Clus.mat')
+    %parsave(subname,FC,mFC,'_FC.mat')
     %% individual static integration component Hin and segragtion component Hse
     [Hin,Hse] =Balance(FC,N,Clus_size,Clus_num);
     IN=[IN;Hin];IM=[IM;Hse];
 end
+
+save("-7", strcat(outputdir, "origin_Hb_static.mat"), 'IN', 'IM');
+
 %% stable FC matrix for long-enough fMRI length
 sFC=corr(Long_fmri);
-parsave('origin',IN,IM,'_HB.mat')
 %% Calibrating the individual static segregation and integration component
 [Hin,Hse] = Stable_correct(sFC,IN,IM,N);
-parsave('corrected',Hin,Hse,'_HB.mat')
-
-
+save("-7", strcat(outputdir, "corrected_Hb_static.mat"), 'Hin', 'Hse');
